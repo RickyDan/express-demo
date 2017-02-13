2a98d228532be3f09d4317675e6dd1bde5e6aeca
RickyDan <ricky@RickyDandeMacBook-Pro-2.local>
2017-02-13 11:46:33 +0800

    实现loading组件

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/loading.css'
+
+export default class Loading extends React.Component {
+  render () {
+    return (
+      <div className="loader">
+        Loading...
+      </div>
+    )
+  }
+}
